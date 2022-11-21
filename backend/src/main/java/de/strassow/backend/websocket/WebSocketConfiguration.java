a0352923bddadff9d95022fa454be0a900ca7632
chris_yooo <hhxtreams@gmail.com>
2022-11-21 17:16:43 +0100

    change react-scripts version

---
+++ backend/src/main/java/de/strassow/backend/websocket/WebSocketConfiguration.java
@@ -0,0 +1,22 @@
+package de.strassow.backend.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    private final ChatService chatService;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(chatService, "/api/mainchat")
+                .setAllowedOrigins("*");
+    }
+}
