6eb0890bdbc15772f0e45dac2300d3ef5225e5ff
chris_yooo <hhxtreams@gmail.com>
2022-11-21 14:48:00 +0100

    add proxy, add security config, add dep for socket

---
+++ backend/src/main/java/de/strassow/backend/SecurityConfig.java
@@ -0,0 +1,25 @@
+package de.strassow.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers(
+                    "/**"
+                ).permitAll()
+                .anyRequest().denyAll()
+                .and().build();
+    }
+}
