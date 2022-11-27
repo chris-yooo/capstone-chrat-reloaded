bac64752ac49b9051ea4513e68bb84e387099398
chris_yooo <hhxtreams@gmail.com>
2022-11-27 12:12:49 +0100

    rewrite tests but need help

--- backend/src/test/java/de/strassow/backend/security/ChratUserControllerTest.java
+++ backend/src/test/java/de/strassow/backend/security/ChratUserControllerTest.java
@@ -37,17 +37,57 @@ class ChratUserControllerTest {
 
     @DirtiesContext
     @Test
-    void loginWithoutCredentials() throws Exception {
+//    void loginWithoutCredentials() throws Exception {
+//
+//        mvc.perform(MockMvcRequestBuilders.get("/api/chrat-users/login"))
+//                .andExpect(status().isUnauthorized());
+//    }
 
-        mvc.perform(MockMvcRequestBuilders.get("/api/chrat-users/login"))
-                .andExpect(status().isUnauthorized());
+    void loginWithTestUser() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/api/chrat-users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {"username": "detlev",
+                                "firstName": "det",
+                                "lastName": "lev",
+                                "email": "test@gmail.com",
+                                "password": "SuperSecret344$$"}
+                                """))
+                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/chrat-users/login")
+                        .header("Authorization", "detlev:SuperSecret344$$")
+                        .session(new MockHttpSession()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("OK"));
     }
 
     @DirtiesContext
     @Test
-    void logout() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.post("/api/chrat-users/logout"))
-                .andExpect(status().isUnauthorized());
+//    void logout() throws Exception {
+//        mvc.perform(MockMvcRequestBuilders.post("/api/chrat-users/logout"))
+//                .andExpect(status().isUnauthorized());
+//    }
+
+    void loginWithTestUserAndLogout() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/api/chrat-users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {"username": "detlev",
+                                "firstName": "det",
+                                "lastName": "lev",
+                                "email": "test@gmail.com",
+                                "password": "SuperSecret344$$"}
+                                """))
+                .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/chrat-users/login")
+                        .header("Authorization", "detlev:SuperSecret344$$")
+                        .session(new MockHttpSession()))
+                .andExpect(status().isOk())
+                .andExpect(content().string("ok"));
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/chrat-users/logout"));
     }
 
     @DirtiesContext
