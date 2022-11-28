46c9a9fb398f852e4cbda356a236bbaede99ed4f
chris_yooo <hhxtreams@gmail.com>
2022-11-28 21:30:54 +0100

    try to use token within handeTextMessage

--- backend/src/main/java/de/strassow/backend/auth/ChratUserTokenRepository.java
+++ backend/src/main/java/de/strassow/backend/auth/ChratUserTokenRepository.java
@@ -2,5 +2,10 @@ package de.strassow.backend.auth;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ChratUserTokenRepository extends MongoRepository<ChratUserToken, String> {
+    Optional<Object> findByUsername(String username);
+
+    void deleteAllByUsername(String username);
 }
