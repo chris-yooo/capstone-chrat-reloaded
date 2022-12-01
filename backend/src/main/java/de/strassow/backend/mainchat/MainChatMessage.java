21d7924393d5e0f0e35957104267106cd55a7d8e
chris_yooo <hhxtreams@gmail.com>
2022-12-01 12:24:07 +0100

    request change fixes

--- backend/src/main/java/de/strassow/backend/mainchat/MainChatMessage.java
+++ backend/src/main/java/de/strassow/backend/mainchat/MainChatMessage.java
@@ -3,14 +3,9 @@ package de.strassow.backend.mainchat;
 import javax.validation.constraints.NotBlank;
 
 public record MainChatMessage(
-
-
         @NotBlank String username,
         @NotBlank String datetime,
         @NotBlank String message
-
-
-
 ) {
     @Override
     public String toString() {
