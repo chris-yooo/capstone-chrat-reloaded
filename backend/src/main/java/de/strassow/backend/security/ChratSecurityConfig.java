333debfd6c2cfb79b05f4861be258e1c1f70adc5
chris_yooo <hhxtreams@gmail.com>
2022-12-07 01:55:55 +0100

    security changes

--- backend/src/main/java/de/strassow/backend/security/ChratSecurityConfig.java
+++ backend/src/main/java/de/strassow/backend/security/ChratSecurityConfig.java
@@ -30,7 +30,7 @@ public class ChratSecurityConfig {
                 .antMatchers(HttpMethod.GET, "/", "/static/**", "/index.html", "/api/chrat-users/me").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/chrat-users").permitAll()
                 .antMatchers(HttpMethod.DELETE, "/api/chrat-users/{id}").authenticated()
-                .antMatchers("/api/mainchat", "/api/chrat-users/login", "/api/chrat-users/logout", "/api/chrat-users/{username}").authenticated()
+                .antMatchers("/api/mainchat", "/api/chrat-users/login", "/api/chrat-users/logout", "/api/chrat-users/{username}", "/api/pictures/*").authenticated()
                 .anyRequest().denyAll()
                 .and().build();
     }
